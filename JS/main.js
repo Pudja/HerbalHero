f65007991624477aa5731afd94dd96e87b5e1fd9
Pudja <45818742+Pudja@users.noreply.github.com>
2024-04-22 16:11:42 +0200

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,27 @@
 function startGame() {
     myGameArea.start();
-    player1 = new component(30, 30, "red", 230, 135)
+    player1 = new component(30, 30, "red", startWidth, startHeight)
 }
+
+var windowHeight = screen.availHeight
+var startHeight = windowHeight / 2
+var windowWidth = screen.availWidth
+var startWidth = windowWidth / 2
+
+console.log(windowHeight)
+console.log(startHeight)
+console.log(windowWidth)
+console.log(startWidth)
+
 
 //Creating canvas and initiating game
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-      this.canvas.width = 480;
-      this.canvas.height = 270;
-      this.canvas.bgcolor = "black";
+      var w = screen.availWidth;
+      var h = screen.availHeight;
+      this.canvas.width = w ;
+      this.canvas.height = h ;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
       this.interval = setInterval(updateGameArea, 20);
@@ -39,30 +51,124 @@ var myGameArea = {
     }    
 
 
-    function updateGameArea() {
-        myGameArea.clear();
-        player1.newPos();
-        player1.update();
-      }
 
 
 
 
 
+
+
+
+
+
+
+
+window.addEventListener("keydown", logKey);
+function logKey(e) {
+    switch (e.key) {
+        case 'w':
+            moveUp()
+            border_control_height()
+            break;
+        case 's':
+            moveDown()
+            border_control_height()
+            break;
+        case 'a':
+            moveLeft()
+            border_control_width()
+            break;
+        case 'd':
+            moveRight()
+            border_control_width()
+            break;
+
+}};
+
+
+
+
+
+
+
+
+
+
+
+// FUNCTIONS USED IN A CODE
+
+//Game state update
+function updateGameArea() {
+    myGameArea.clear();
+    player1.newPos();
+    player1.update();
+}
+
+
+//Movement
 function moveUp() {
     player1.speedY -= 1;
-    console.log(this.player1.y)
-
+    window.addEventListener("keyup", stop);
 }
 function moveDown() {
     player1.speedY += 1;
-    console.log(this.player1.y)
+    window.addEventListener("keyup", stop);
 }
 function moveLeft() {
     player1.speedX -= 1;
-    console.log(this.player1.x)
+    window.addEventListener("keyup", stop);
 }
 function moveRight() {
     player1.speedX += 1;
-    console.log(this.player1.x)
+    window.addEventListener("keyup", stop);
+}
+
+//Stop on realease
+function _moveUp() {
+    player1.speedY = 0;
+}
+function _moveDown() {
+    player1.speedY  = 0;
+}
+function _moveLeft() {
+    player1.speedX = 0;
+}
+function _moveRight() {
+    player1.speedX = 0;
+}
+function stop(e) {
+    switch (e.key) {
+        case 'w':
+            _moveUp()
+            console.log(player1.x)
+            break;
+        case 's':
+            _moveDown()
+            console.log(player1.x)
+            break;
+        case 'a':
+            _moveLeft()
+            console.log(player1.y)
+            break;
+        case 'd':
+            _moveRight()
+            console.log(player1.y)
+            break;
+
+}};
+//Border Control (inbsert mexico joke)
+function border_control_width (x){
+    x = player1.x
+    if(x == screen.availWidth){
+        player1.x = 0;
+        console.log(x)
+    }
+    
+}
+function border_control_height (y){
+    y = player1.y
+    if(y == screen.availWidth){
+        player1.y = 0;
+        console.log(y)
+    }
 }
